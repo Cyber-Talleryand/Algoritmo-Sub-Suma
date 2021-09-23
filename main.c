0b2c35a6d59ff7aeaef1a63782337027f7a918f3
Cyber-Talleryand <82671518+Cyber-Talleryand@users.noreply.github.com>
2021-09-23 15:30:29 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <math.h>
 #include <sys/time.h>
 
+#define w 100
+
 
 double microsegundos() { /* obtiene la hora del sistema en microsegundos */
     struct timeval t;
@@ -13,7 +15,7 @@ double microsegundos() { /* obtiene la hora del sistema en microsegundos */
     return (t.tv_usec + t.tv_sec * 1000000.0);
 }
 
-void sumaSubMax1 (int v[], int N){
+int sumaSubMax1 (int v[], int N){
     int i, j;
     int sumaMax = 0;
     int estaSuma;
@@ -24,9 +26,10 @@ void sumaSubMax1 (int v[], int N){
             if (estaSuma > sumaMax) sumaMax = estaSuma;
         }
     }
+    return sumaMax;
 }
 
-void sumaSubMax2 (int v[], int N){
+int sumaSubMax2 (int v[], int N){
     int i;
     int sumaMax = 0;
     int estaSuma = 0;
@@ -39,6 +42,7 @@ void sumaSubMax2 (int v[], int N){
             estaSuma = 0;
         }
     }
+    return sumaMax;
 }
 
 void inicializar_semilla(){
@@ -60,9 +64,48 @@ void listar_vector(int v[], int N){
     }
     printf("\t");
 }
+void test1(){
+    int x, y,k;
+    int v[] = {7, 7, -5, -7, 6};
+    printf("\ntest1\n");
+    printf("%30s%15s%15s\n", "", "sumaSubMax1", "sumaSubMax2");
+    k= sizeof(v)/ sizeof(int);
+    listar_vector(v,k);
+    x = sumaSubMax1(v,k);
+    y = sumaSubMax2(v,k);
+    printf("%15d%15d\n\n", x, y);
+}
+
+void test2(){
+    int i, x, y,N=5;
+    int v[N];
+    printf("test2\n");
+    printf("%30s%15s%15s\n", "", "sumaSubMax1", "sumaSubMax2");
+    for (i = 0; i < N; i++){
+        aleatorio(v,N);
+        listar_vector(v,N);
+        x = sumaSubMax1(v,N);
+        y = sumaSubMax2(v,N);
+        printf("%15d%15d\n", x, y);
+    }
+}
+
+void test_propio(){
+    int x, y,k,v[30]={-9,2,-5,-4,6,4,0,9,2,5,-2,-1,-9,-7,-1,9,-2,1,-7,-8,15,-2,-5,-4,16,7,-5,6,7,-7};
+    printf("\ntest propio\n");
+    printf("%30s%15s%15s\n", "", "sumaSubMax1", "sumaSubMax2");
+        k= sizeof(v)/ sizeof(int);
+        listar_vector(v,k);
+        x = sumaSubMax1(v,k);
+        y = sumaSubMax2(v,k);
+        printf("%15d%15d\n", x, y);
+
+    }
+
+
 void test_recursivo_n1(){
     int v[32000],c[7]={500,1000,2000,4000,8000,16000,32000},i,j;
-    double a,b,f_c,w=100;
+    double a,b,f_c,d;
     printf("\n\n%8s%8s%8s%16s%16s%16s\n\n", "n", "", "t(n)", "t(n)/n^1.8", "t(n)/n^2", "t(n)/n^2.2");
     for(j=0;j<6;j++) {
         for (i = 0; i < 7; i++) {
@@ -78,8 +121,7 @@ void test_recursivo_n1(){
                 printf("%f\t%f\t%f\t%f\n", b - a, (b-a)/pow(f_c,1.8), (b-a)/pow(f_c,2), (b-a)/pow(f_c,2.2));
             }           
             else{
-                double d;
-                printf("sec %d %d %s\t", j, c[i], "(*)");
+                printf("sec %d %s\t", c[i], "(*)");
                 a = microsegundos();
                 //printf("%f\t", a = microsegundos());
                 for (int z = 0; z < w; z++) {
@@ -97,11 +139,11 @@ void test_recursivo_n1(){
 
 void test_recursivo_n2(){
     int v[32000],c[7]={500,1000,2000,4000,8000,16000,32000},i,j;
-    double a,b,w=100,f_c,d;
+    double a,b,f_c,d;
     printf("\n\n%8s%16s%16s%16s%16s\n\n", "n", "t(n)", "t(n)/n^0.8", "t(n)/n^1", "t(n)/n^1.2");
     for(j=0;j<6;j++) {
         for (i = 0; i < 7; i++) { 
-                printf("sec %d %d %s\t", j, c[i], "(*)");
+                printf("sec %d %s\t", c[i], "(*)");
                 a = microsegundos();
                 //printf("%f\t", a = microsegundos());
                 for (int z = 0; z < w; z++) {
@@ -118,6 +160,7 @@ void test_recursivo_n2(){
 int main(){
 
     inicializar_semilla();
+    //test_propio();
     test_recursivo_n1();
     test_recursivo_n2();
 
